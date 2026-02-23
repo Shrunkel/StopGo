82d24abd072579a815435aa917d85faeacaee669
Shrunkel <kayreymaistry5@gmail.com>
2026-02-24 00:53:37 +0200

    First commit

---
+++ src/signs/Signs.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package signs;
+
+/**
+ *
+ * @author Sketchy_Humour
+ */
+public class Signs {
+
+
+    public static void main(String[] args) {
+        System.out.println("                   -----------");           //this is the top section
+        System.out.println("   ******** \t\t\t     XXXXXXXX");
+        System.out.println(" *          * \t\t\t   X\t      X");        /*code indentation was made with \t*/
+        System.out.println("*   S T O P  * \t\t\t  X\tGO     X");       //STOP and GO words for the sign
+        System.out.println(" *          * \t\t\t   X\t      X");
+        System.out.println("   ******** \t\t\t     XXXXXXXX");
+        System.out.println("      ** \t\t\t\tXX");
+        System.out.println("      ** \t\t\t\tXX");
+        System.out.println("      ** \t\t\t\tXX");
+        System.out.println("      ** \t\t\t\tXX");
+        System.out.println("     ****\t\t\t       XXXX");
+        System.out.println("  ********** \t\t\t    XXXXXXXXXX");
+        System.out.println("|------------------------------------------------|");
+        System.out.println("|------------------------------------------------|");   //this is the bottom section
+        
+          
+    }
+    
+}
